92863f755ebe0add0dfa8c72aa61fc9f3f2e1312
Pratik Gupta <pratikguptamain@gmail.com>
2025-06-30 21:43:21 +0530

    feat: implement initial user signup controller with necessary imports

---
+++ ai-ticket-assistant/controllers/user.js
@@ -0,0 +1,8 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+import { inngest } from '../inngest/client.js';
+
+export const signup = async (req, res) => {
+    
+}
