0510df348f2ec1053543fe04f4143c5a57cc9822
Pratik Gupta <pratikguptamain@gmail.com>
2025-07-02 15:22:04 +0530

    Implement ticket creation function with AI analysis and email notification

--- ai-ticket-assistant/inngest/functions/on-ticket-create.js
+++ ai-ticket-assistant/inngest/functions/on-ticket-create.js
@@ -1,10 +1,9 @@
-import { inngest } from "../client";
+import { inngest } from "../client.js";
 import Ticket from "../../models/ticket.js";
 import { NonRetriableError } from "inngest";
 import { sendMail } from "../../utils/mailer.js";
-import analyzeTicket from './../../utils/ai';
+import analyzeTicket from './../../utils/ai.js';
 import User from "../../models/user.js";
-import { sendMail } from './../../utils/mailer';
 
 export const onTicketCreated = inngest.createFunction(
     { id: "on-ticket-created", retries: 3},
