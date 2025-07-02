70a5e257676586d3ab3e9dc62c475029f447b6d4
Pratik Gupta <pratikguptamain@gmail.com>
2025-07-02 19:28:46 +0530

    Add ticket routes and Inngest integration for user signup and ticket creation

--- ai-ticket-assistant/index.js
+++ ai-ticket-assistant/index.js
@@ -2,7 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import { serve } from "inngest/express";
 import userRoutes from "./routes/user.js";
+import ticketRoutes from "./routes/ticket.js";
+import { inngest } from "./inngest/client.js";
+import { onUserSignup } from "./inngest/functions/on-signup.js";
+import { onTicketCreated } from "./inngest/functions/on-ticket-create.js";
 
 
 dotenv.config();
@@ -14,6 +19,15 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
+app.use("/api/tickets", ticketRoutes);
+
+app.use(
+    "/api/inngest",
+    serve({
+        client: inngest,
+        functions: [onUserSignup, onTicketCreated]
+    })
+)
 
 mongoose
     .connect(process.env.MONGO_URI)
