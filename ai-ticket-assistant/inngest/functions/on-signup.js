d4e91e1795404fb9cbcdcd0831828cf8ad079ab3
Pratik Gupta <pratikguptamain@gmail.com>
2025-07-03 16:19:04 +0530

    fix: correct import statement for inngest client in on-signup function

--- ai-ticket-assistant/inngest/functions/on-signup.js
+++ ai-ticket-assistant/inngest/functions/on-signup.js
@@ -1,4 +1,4 @@
-import { inngest } from "../client";
+import { inngest } from "../client.js";
 import User from "../../models/user.js";
 import { NonRetriableError } from "inngest";
 import { sendMail } from "../../utils/mailer.js";
